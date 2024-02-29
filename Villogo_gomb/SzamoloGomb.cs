972950506847dcc76763b8ea1f90207b7bce47d2
Tomi489 <tomi.rutai@gmail.com>
2024-02-29 14:35:23 +0100

    er

---
+++ Villogo_gomb/SzamoloGomb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villogo_gomb
+{
+    internal class SzamoloGomb : Button
+    {
+        int szam = 0;               
+        public SzamoloGomb()
+        {
+                Height = 20;
+                Width = 20;
+            MouseClick += SzamoloGomb_MouseClick;
+                
+        }
+
+        private void SzamoloGomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            
+            if(szam==5)
+            {
+                szam = 0;
+            }
+            szam++;
+            Text =szam.ToString();
+        }
+    }
+}
